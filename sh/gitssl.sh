4c66a8cee1af19473bf11f295b7f1cf5a5e052be
故梦长忆 <43817511+MemoryLong@users.noreply.github.com>
2024-07-28 22:44:21 +0800

    Update gitssl.sh

--- sh/gitssl.sh
+++ sh/gitssl.sh
@@ -4,16 +4,16 @@
 #sudo chmod +x gitssl.sh
 # 变量
 KEYNAME="webserver"
-
+sudo -i
 # 生成新的 SSH 密钥
 ssh-keygen -t rsa -b 4096 -C "haifeng222222@sina.com" -f /root/.ssh/$KEYNAME_rsa
 
 # 添加 SSH 密钥到 SSH 代理
 eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/$KEYNAME
+ssh-add /root/.ssh/$KEYNAME_rsa
 
 # 打开 id_rsa.pub 文件并复制内容
-cat ~/.ssh/$KEYNAME.pub
+cat /root/.ssh/$KEYNAME_rsa.pub
 # 登录到你的 GitHub 账户。
 # 导航到 Settings -> SSH and GPG keys -> New SSH key。
 # 粘贴你的公钥内容并保存。
