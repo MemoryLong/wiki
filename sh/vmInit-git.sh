f49828d719857bf53de1447a6e153253b5eac6e9
故梦长忆 <43817511+MemoryLong@users.noreply.github.com>
2024-07-30 22:40:11 +0800

    Update vmInit-git.sh

--- sh/vmInit-git.sh
+++ sh/vmInit-git.sh
@@ -9,8 +9,21 @@ sudo -i
 ssh-keygen -t rsa -b 4096 -C "haifeng222222@sina.com" -f /root/.ssh/$KEYNAME_rsa
 
 # 添加 SSH 密钥到 SSH 代理
-eval "$(ssh-agent -s)"
-ssh-add /root/.ssh/$KEYNAME_rsa
+#eval "$(ssh-agent -s)"
+#ssh-add /root/.ssh/$KEYNAME_rsa
+# 以下是一个完整的 /root/.ssh/config 示例，其中包含多个主机的配置：
+# Host github.com
+#   HostName github.com
+#   User git
+#   IdentityFile ~/.ssh/id_rsa
+#   IdentitiesOnly yes
+
+# 配置 GitLab
+# Host gitlab.com
+#   HostName gitlab.com
+#   User git
+#   IdentityFile ~/.ssh/id_rsa_gitlab
+#   IdentitiesOnly yes
 
 # 打开 id_rsa.pub 文件并复制内容
 cat /root/.ssh/$KEYNAME_rsa.pub
